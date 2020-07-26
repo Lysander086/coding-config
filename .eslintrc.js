d80710ff2546f58cf5bfb85eca6de0cb38823b44
Lysander <19286863+Lysander686@users.noreply.github.com>
2020-07-26 14:53:04 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,17 @@
-{
-  "extends": "standard",
-  "plugins": [
-    "html"
-  ],
-  "parser": "babel-eslint",
-  // babel处理的语法, eslint可能不太支持
-  "rules": {
-    "no-new": "off",
-    // allow paren-less arrow functions
-    "arrow-parens": 0,
-    // allow async-await
-    "generator-star-spacing": 0,
-    // allow debugger during development
-    "no-console": 1,
-    "no-debugger": 1,
-    "no-unused-vars": [
-      1,
-      {
-        // 允许声明未使用变量
-        "vars": "local",
-        // 参数不检查
-        "args": "none"
-      }
-    ],
-    // 关闭语句强制分号结尾
-    "semi": [
-      0
-    ],
-    //空行最多不能超过100行
-    "no-multiple-empty-lines": [
-      0,
-      {
-        "max": 10
-      }
-    ],
-    //关闭禁止混用tab和空格
-    "no-mixed-spaces-and-tabs": [
-      0
-    ],
-    "space-before-function-paren": "off",
-    "no-extra-semi": "off"
+// source: https://blog.csdn.net/shaleilei/article/details/89668801
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  // extends: ["plugin:vue/essential", "@vue/prettier"],//这里面的@vue/prettier去掉
+  extends: ["plugin:vue/essential"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
+  parserOptions: {
+    // parser: "babel-eslint"
   }
-}
+};
+
